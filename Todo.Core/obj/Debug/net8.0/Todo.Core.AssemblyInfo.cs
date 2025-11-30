f4cc2a04683f7d90b59f6598eb99244b536abe85
helluvaFireLord <firelord2k6y@gmail.com>
2025-11-30 23:12:20 +0700

    extra task commit

--- Todo.Core/obj/Debug/net8.0/Todo.Core.AssemblyInfo.cs
+++ Todo.Core/obj/Debug/net8.0/Todo.Core.AssemblyInfo.cs
@@ -11,13 +11,14 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Todo.Core")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Student")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00091e3180ad0dc38e93123ccf79899fcccdc0e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/USERNAME/REPOSITORY")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
